3833bddf260a3d473ed99525c4d69cba92c5566b
edgardo-padovan <edgardopadovan@gmail.com>
2023-07-07 09:51:49 -0300

    crud alternativo

---
+++ front/js/productos.js
@@ -0,0 +1,48 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+           productos: [],
+           url: 'https://edgardopadovan.pythonanywhere.com/productos',
+           cargando: true,
+           error: false
+        }
+    },
+
+    methods: {
+    
+       fetchApi(url){
+        fetch(url)
+        .then(res => res.json())
+        .then(data =>{
+            this.productos = data;
+            this.cargando = false;
+        })
+        .catch(err=>{
+            console.error(err);
+            this.error = true;
+
+        })
+       },
+       
+       eliminar(id){
+        const url = this.url+"/"+id
+        let options = {
+            method: 'DELETE'
+        }
+
+        fetch(url,options)
+        .then(res => res.json)
+        .then(data =>{
+            location.reload();
+        })
+        .catch(err => console.error(err))
+       }
+    },
+
+    created(){
+        this.fetchApi(this.url);
+    }
+
+}).mount('#app')
